0b395afdec317e277bf57144ae959992d115586f
Vinicius Gomes de Oliveira <vinicius.oliveira@intelbras.com.br>
2020-09-20 22:24:57 -0300

    Finalizando projeto

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -1,27 +1,37 @@
-const Dev = require('../models/Dev');
+const Dev = require('../models/Dev.js');
 
 module.exports = {
-   async store(req, resp){
-
-        const { devId} = req.params;
-        const { user } = req.headers;
+    async store(request, response) {
+        const { devId } = request.params;
+        const { user } = request.headers;
 
         const loggedDev = await Dev.findById(user);
         const targetDev = await Dev.findById(devId);
 
-        if(!targetDev){
-            return resp.status(400).json({error: 'Dev not exists'});
-        }
-
-        if(targetDev.likes.includes(loggedDev._id)){
-            console.log('DEU MATCH!')
+        if (!targetDev) {
+            return response.status(400).json({ error: 'Dev not found'});
         }
 
         loggedDev.likes.push(targetDev._id);
 
         await loggedDev.save();
 
+        console.log(`User ${loggedDev.user} liked ${targetDev.user}`);
 
-        return resp.json(loggedDev);
-    } 
+        if (targetDev.likes.includes(loggedDev._id)) {
+            console.log('QUE MASSA DEU MATCH!');
+            const loggedSocket = request.connectedUsers[user];
+            const targetSocket = request.connectedUsers[devId];
+
+            if (loggedSocket) {
+                request.io.to(loggedSocket).emit('match', targetDev);
+            }
+
+            if (targetSocket) {
+                request.io.to(targetSocket).emit('match', loggedDev);
+            }
+        }
+
+        return response.json(loggedDev);
+    }
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,19 @@ const cors = require('cors');
 
 const routes = require('./routes');
 
-const server = express();
+const app = express();
+const server = require('http').Server(app); 
+const io = require('socket.io')(server);
+
+const connectedUsers = {}
+
+io.on('connection', socket => {
+    const { user } = socket.handshake.query
+    connectedUsers[user] = socket.id
+    console.log('Client connectet:', user )
+
+});
+ 
 
 mongoose.connect('mongodb://mongoadmin:secret@localhost:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false', {
   useNewUrlParser: true,
@@ -12,8 +24,15 @@ mongoose.connect('mongodb://mongoadmin:secret@localhost:27017/?authSource=admin&
   useUnifiedTopology: true,
 });
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
